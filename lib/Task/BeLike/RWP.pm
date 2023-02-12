b95de15813135919021f0bc1021703c62847bdfa
rwp0 <rwp.primary@gmail.com>
2023-02-12 22:00:28 +0400

    docs: list modules

--- lib/Task/BeLike/RWP.pm
+++ lib/Task/BeLike/RWP.pm
@@ -1,5 +1,26 @@
-use strict;
-use warnings;
+use v5.37.9;
+
 package Task::BeLike::RWP;
 
-1;
+=head1 DESCRIPTION
+
+=head2 MODULES
+
+The following Perl modules will be installed as part of this bundle.
+
+=for :list
+* L<Path::Tiny>
+* L<System::Command>
+* L<Net::DNS>
+* L<Dist::Zilla>
+* L<Config::General>
+* L<DBI>
+* L<SQL::Abstract>
+* L<URI>
+* L<Clipboard>
+* L<PadWalker>
+* L<Data::Printer>
+* L<Log::Log4Perl>
+* L<IO::Tee>
+* L<Domain::PublicSuffix>
+* L<Net::IP::XS>
