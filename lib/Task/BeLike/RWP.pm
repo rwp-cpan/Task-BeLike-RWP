09188ea7446db3da9e76213afefb282ca0547d6b
rwp0 <rwp.primary@gmail.com>
2023-02-16 18:36:19 +0400

    Fix Log-Log4perl name

--- lib/Task/BeLike/RWP.pm
+++ lib/Task/BeLike/RWP.pm
@@ -13,18 +13,35 @@ package Task::BeLike::RWP;
 The following Perl modules will be installed as part of this bundle.
 
 =for :list
-* L<Path::Tiny>
-* L<System::Command>
-* L<Net::DNS>
-* L<Dist::Zilla>
-* L<Config::General>
-* L<DBI>
-* L<SQL::Abstract>
-* L<URI>
-* L<Clipboard>
-* L<PadWalker>
-* L<Data::Printer>
-* L<Log::Log4Perl>
-* L<IO::Tee>
-* L<Domain::PublicSuffix>
-* L<Net::IP::XS>
+= L<Path::Tiny> (L<GitHub|https://github.com/dagolden/Path-Tiny>)
+OOP file management
+= L<System::Command> (L<GitHub|https://github.com/book/System-Command>)
+Execute commands in OOP style
+= L<Net::DNS> (L<GitHub|https://github.com/NLnetLabs/Net-DNS>)
+OOP DNS resolver and the rest
+= L<Dist::Zilla> (L<GitHub|https://github.com/rjbs/Dist-Zilla>)
+Perl distribution management and release
+= L<Config::General> (L<GitHub|https://github.com/TLINDEN/Config-General>)
+General configuration file parser
+= L<DBI> (L<GitHub|>)
+Database connection and manipulation
+= L<SQL::Abstract> (L<GitHub|>)
+SQL command abstraction in OOP style with useful statement costructing methods
+= L<URI> (L<GitHub|>)
+General URI class to handle URL's and the like
+= L<Clipboard> (L<GitHub|>)
+Cross-platform clipboard management
+= L<PadWalker> (L<GitHub|>)
+Supported by Perl Debugger as the C<y> command
+= L<Data::Printer> (L<GitHub|>)
+Aids in debugging by displaying Perl data structures
+= L<Log::Log4Perl> (L<GitHub|>)
+Perl logging framework
+= L<IO::Tee> (L<GitHub|>)
+Tee file handle
+= L<Domain::PublicSuffix> (L<GitHub|>)
+Get to the domain root, by Mozilla
+= L<Net::IP::XS> (L<GitHub|>)
+IP class
+= L<XML::RSS> (L<GitHub|>)
+RSS parser
