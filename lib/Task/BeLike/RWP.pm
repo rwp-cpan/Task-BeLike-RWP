805194b0f6952a1c866910c818dc4a1499d60ca1
rwp0 <rwp.primary@gmail.com>
2023-06-23 12:38:40 +0200

    Fix POD formatting
    https://metacpan.org/release/RWP/Task-BeLike-RWP-0.231740/view/lib/Task/BeLike/RWP.pm

--- lib/Task/BeLike/RWP.pm
+++ lib/Task/BeLike/RWP.pm
@@ -19,57 +19,38 @@ package Task::BeLike::RWP;
 The following Perl modules will be installed as part of this bundle.
 
 =for :list
-
 = L<Path::Tiny> (L<GitHub|https://github.com/dagolden/Path-Tiny>)
 OOP file management
-
 = L<System::Command> (L<GitHub|https://github.com/book/System-Command>)
 Execute commands in OOP style
-
 = L<Net::DNS> (L<GitHub|https://github.com/NLnetLabs/Net-DNS>)
 OOP DNS resolver and the rest
-
 = L<Dist::Zilla> (L<GitHub|https://github.com/rjbs/Dist-Zilla>)
 Perl distribution management and release
-
 = L<Config::General> (L<GitHub|https://github.com/TLINDEN/Config-General>)
 General configuration file parser
-
 = L<DBI> (L<GitHub|https://github.com/perl5-dbi/dbi>)
 Database connection and manipulation
-
 = L<SQL::Abstract> (L<GitHub|https://github.com/dbsrgits/sql-abstract>)
 SQL command abstraction in OOP style with useful statement costructing methods
-
 = L<URI> (L<GitHub|https://github.com/libwww-perl/URI>)
 General URI class to handle URL's and the like
-
 = L<Clipboard> (L<GitHub|https://github.com/shlomif/Clipboard>)
 Cross-platform clipboard management
-
 = L<PadWalker> (L<GitHub|https://github.com/robinhouston/PadWalker>)
 Supported by Perl Debugger as the C<y> command
-
 = L<Data::Printer> (L<GitHub|https://github.com/garu/Data-Printer>)
 Aids in debugging by displaying Perl data structures
-
 = L<Log::Log4perl> (L<GitHub|https://github.com/mschilli/log4perl>)
 Perl logging framework
-
 = L<IO::Tee> (L<GitHub|https://github.com/neilb/IO-Tee>)
 Tee file handle
-
 = L<Domain::PublicSuffix> (L<GitHub|https://github.com/nmelnick/domain-publicsuffix>)
 Get to the domain root, by Mozilla
-
 = L<Net::IP::XS> (L<GitHub|https://github.com/tomhrr/p5-Net-IP-XS>)
 IP class
-
 = L<XML::RSS> (L<GitHub|https://github.com/shlomif/perl-XML-RSS>)
 RSS parser. Requires the C<libexpat1-dev> package to be installed.
-
 = L<Yaml::XS> (L<GitHub|https://github.com/ingydotnet/yaml-libyaml-pm>>)
-
-= L<Rex>= (L<GitHub|https://github.com/RexOps/Rex>)
-
+= L<Rex> (L<GitHub|https://github.com/RexOps/Rex>)
 = L<Number::Bytes::Human> (L<GitHub|https://github.com/aferreira/cpan-Number-Bytes-Human)
